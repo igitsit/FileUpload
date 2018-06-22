f2dbbc8607848898ac60eea91460f79e14df34f9
Fahad Ahmad <Fahad.Ahmad@bcstechnology.com.au>
2018-06-22 16:44:49 +0530

    Add project files.

---
+++ FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,54 @@
+﻿using System;  
+using System.Collections.Generic;  
+using System.IO;  
+using System.Linq;  
+using System.Web;  
+using System.Web.Mvc;
+using FileUpload.Models;
+namespace FileUpload.Controllers  
+{  
+    public class UploadController : Controller
+{
+    // GET: Upload  
+    public ActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public ActionResult UploadFile()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ActionResult UploadFile(Image Img,  HttpPostedFileBase file )
+    {
+        try
+        {  
+            Image imageModel = new Image();
+            
+            if (file.ContentLength > 0)
+            {
+                string _FileName = Path.GetFileName(file.FileName);
+                string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                imageModel.ImagePath = _path;
+                imageModel.FileName = _FileName;
+                imageModel.Title = Img.Title;
+                file.SaveAs(_path);
+                
+            }
+                using (ImageEntities db = new ImageEntities())
+                {   
+                    db.Images.Add(imageModel);
+                    db.SaveChanges();
+                }
+                    ViewBag.Message = "File Uploaded Successfully!!";
+                     return View();
+        }
+        catch
+        {
+            ViewBag.Message = "File upload failed!!";
+            return View();
+        }
+    }
+}  
+}  
